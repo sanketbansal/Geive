89454d6056ac2cf1d5c853a9925e225fc67ad79b
Unknown <sanketbansal57@gmail.com>
2018-04-09 18:39:51 +0530

    Test module
    Testing files updated

---
+++ app/src/test/java/com/example/root/proto2/IncomingHandlerTest.java
@@ -0,0 +1,20 @@
+package com.example.root.proto2;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sanket on 11/25/2017.
+ */
+public class IncomingHandlerTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+}
